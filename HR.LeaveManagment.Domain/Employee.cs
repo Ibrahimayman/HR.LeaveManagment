ea3730d6ab6e528f0293b37d14c029036222d1fb
Ibrahim <eng.ibrahim.cer@hotmail.com>
2021-10-12 17:57:21 +0200

    first commit

---
+++ HR.LeaveManagment.Domain/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HR.LeaveManagment.Domain
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
