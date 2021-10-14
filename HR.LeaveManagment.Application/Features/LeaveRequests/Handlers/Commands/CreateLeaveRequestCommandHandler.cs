11c34873ccd14b8422db48e81b5773d911803f67
Ibrahim <eng.ibrahim.cer@hotmail.com>
2021-10-14 13:45:26 +0200

    CQRS

---
+++ HR.LeaveManagment.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using HR.LeaveManagment.Application.Features.LeaveRequests.Requests.Commands;
+using System.Threading.Tasks;
+using System.Threading;
+using HR.LeaveManagment.Application.Persistence.Contracts;
+using AutoMapper;
+using HR.LeaveManagment.Domain;
+
+namespace HR.LeaveManagment.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, int>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var LeaveRequest = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
+            LeaveRequest = await _leaveRequestRepository.Add(LeaveRequest);
+            return LeaveRequest.Id;
+        }
+    }
+}
---
+++ HR.LeaveManagment.Application/Features/LeaveRequests/Requests/Commands/CreateLeaveRequestCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HR.LeaveManagment.Application.DTOs.LeaveRequest;
+using MediatR;
+
+namespace HR.LeaveManagment.Application.Features.LeaveRequests.Requests.Commands
+{
+    public class CreateLeaveRequestCommand : IRequest<int>
+    {
+        public LeaveRequestDto LeaveRequestDto { get; set; }
+    }
+}
